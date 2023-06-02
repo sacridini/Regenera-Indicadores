4cb619fb619a15342c5dc50dade380f83c59632f
sacridini <eduardolacerdageo@gmail.com>
2023-06-01 23:03:58 -0300

    small update in split_rasters.R

--- split_rasters.r
+++ split_rasters.r
@@ -2,8 +2,7 @@ library(terra)
 
 files <- list.files("C:/Users/Eduardo/Desktop/bd_cata/results/", pattern = "*.tif", full.names = TRUE)
 
-
-for (i in 1:length(files)) {
+for (i in seq_along(files)) {
   r <- rast(files[i])
   for(j in 1:nlyr(r)) {
     writeRaster(r[[j]], paste0("C:/Users/Eduardo/Desktop/bd_cata/results/", names(r[[j]]), ".tif"), gdal = "COMPRESS=DEFLATE")
